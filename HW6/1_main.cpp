9fd2f69389bc6ec43d6ea9cfdc6f493751ed336c
Avacato <sim.in.ua@gmail.com>
2023-11-19 01:25:11 +0200

    add HW6/1_main.cpp

---
+++ HW6/1_main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int getNum();
+
+int main()
+{
+	std::cout << "Enter the first element of progression: ";
+	int firstNum{ getNum() };
+
+	std::cout << "Enter the step for the arithmetic progression: ";
+	int arithmeticProg{ getNum() };
+
+	std::cout << "Enter the last element of progression: ";
+	int lastNum{ getNum() };
+	int currentNumber = firstNum;
+
+	if (firstNum > lastNum) {
+		std::cout << "The first number of an arithmetic sequence cannot be less than the last." << std::endl;
+		return 0;
+	}
+	else if (arithmeticProg > lastNum) {
+		std::cout << "The step number of the arithmetic sequence cannot be less than the last one." << std::endl;
+		return 0;
+	}
+
+	while (currentNumber <= lastNum)
+	{
+		std::cout << currentNumber << " ";
+		currentNumber += arithmeticProg;
+	}
+
+	return 0;
+}
+
+int getNum() {
+	int a;
+	std::cin >> a;
+	return a;
+}
