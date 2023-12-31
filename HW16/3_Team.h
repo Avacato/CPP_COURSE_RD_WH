5c6f65c1fe32ba6ad95f1b3583ea884f62c2e049
Avacato <sim.in.ua@gmail.com>
2023-12-31 23:40:55 +0200

    update 3_Team

--- HW16/3_Team.h
+++ HW16/3_Team.h
@@ -1,44 +1,18 @@
 #pragma once
 #include <vector>
 #include "3_Const.h"
+#include "3_Player.h"
 
 class Team {
 public:
-    Team(const std::string& name) : m_teamName(name), m_numPlayers(0) {}
+    Team(const std::string& name);
 
-    bool addPlayer(Player* newPlayer) {
-        if (m_numPlayers < MAX_PLAYERS) {
-            m_players.push_back(newPlayer);
-            m_numPlayers++;
-            return true;
-        }
-        else {
-            std::cout << "Team is full. Cannot add more players.\n";
-            return false;
-        }
-    }
+    bool addPlayer(Player* newPlayer);
 
-    bool removePlayer(Player* playerToRemove) {
-        auto it = std::find(m_players.begin(), m_players.end(), playerToRemove);
-        if (it != m_players.end()) {
-            m_players.erase(it);
-            m_numPlayers--;
-            return true;
-        }
-        else {
-            std::cout << "Player not found in the team.\n";
-            return false;
-        }
-    }
+    bool removePlayer(Player* playerToRemove);
 
-    void printTeamInfo() const {
-        std::cout << "Team Name: " << m_teamName << "\n";
-        std::cout << "Number of Players: " << m_numPlayers << "\n";
-        for (const auto& player : m_players) {
-            player->printPlayerInfo();
-            std::cout << "\n";
-        }
-    }
+    void printTeamInfo() const;
+
 private:
     std::string m_teamName;
     int m_numPlayers;
